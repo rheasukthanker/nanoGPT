4cb35745e8d4c4e93025e4c036b205f9a346806a
Rhea Sukthanker <rheasukthanker@gmail.com>
2023-02-28 14:50:25 +0100

    (feat): add nanogpt scheduler/optimizer

--- tutorial_code/char_gpt.py
+++ tutorial_code/char_gpt.py
@@ -4,11 +4,19 @@ import matplotlib.pyplot as plt
 import pickle
 from tqdm import tqdm
 
+import tiktoken
+import math
+import inspect
+from dataclasses import dataclass
+
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-import tiktoken
-
+warmup_iters = 100
+lr_decay_iters = 1000
+min_lr = 6e-5
+learning_rate = 6e-4
+decay_lr = True
 def to_tokens(example):
     """Function to tokenize a string using BPE.
     """
@@ -39,7 +47,64 @@ def plot_losses(losses, verbosity, val_losses=None):
     plt.xlim(0, len(losses))
     plt.show();
 
+def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
+        """
+        This long function is unfortunately doing something very simple and is being very defensive:
+        We are separating out all parameters of the model into two buckets: those that will experience
+        weight decay for regularization and those that won't (biases, and layernorm/embedding weights).
+        We are then returning the PyTorch optimizer object.
+        """
 
+        # separate out all parameters to those that will and won't experience regularizing weight decay
+        decay = set()
+        no_decay = set()
+        whitelist_weight_modules = (torch.nn.Linear, )
+        blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
+        for mn, m in self.named_modules():
+            for pn, p in m.named_parameters():
+                fpn = '%s.%s' % (mn, pn) if mn else pn # full param name
+                # random note: because named_modules and named_parameters are recursive
+                # we will see the same tensors p many many times. but doing it this way
+                # allows us to know which parent module any tensor p belongs to...
+                if pn.endswith('bias'):
+                    # all biases will not be decayed
+                    no_decay.add(fpn)
+                elif pn.endswith('weight') and isinstance(m, whitelist_weight_modules):
+                    # weights of whitelist modules will be weight decayed
+                    decay.add(fpn)
+                elif pn.endswith('weight') and isinstance(m, blacklist_weight_modules):
+                    # weights of blacklist modules will NOT be weight decayed
+                    no_decay.add(fpn)
+
+        # subtle: 'transformer.wte.weight' and 'lm_head.weight' are tied, so they
+        # will appear in the no_decay and decay sets respectively after the above.
+        # In addition, because named_parameters() doesn't return duplicates, it
+        # will only return the first occurence, key'd by 'transformer.wte.weight', below.
+        # so let's manually remove 'lm_head.weight' from decay set. This will include
+        # this tensor into optimization via transformer.wte.weight only, and not decayed.
+        decay.remove('lm_head.weight')
+
+        # validate that we considered every parameter
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        inter_params = decay & no_decay
+        union_params = decay | no_decay
+        assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params), )
+        assert len(param_dict.keys() - union_params) == 0, "parameters %s were not separated into either decay/no_decay set!" \
+                                                    % (str(param_dict.keys() - union_params), )
+
+        # create the pytorch optimizer object
+        optim_groups = [
+            {"params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": weight_decay},
+            {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0},
+        ]
+        # new PyTorch nightly has a new 'fused' option for AdamW that is much faster
+        use_fused = (device_type == 'cuda') and ('fused' in inspect.signature(torch.optim.AdamW).parameters)
+        print(f"using fused AdamW: {use_fused}")
+        extra_args = dict(fused=True) if use_fused else dict()
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
+
+        return optimizer
+    
 def get_batch( split: str, block_size: int = 8, batch_size: int = 4, device: str = None):
     """ Gets a randomized batch from the split of data chosen.
 
@@ -137,6 +202,71 @@ def train_and_evaluate_model(
     if plot_loss:
         plot_losses(train_losses, verbosity_len, valid_losses)
 
+
+
+def get_lr(it):
+    # 1) linear warmup for warmup_iters steps
+    if it < warmup_iters:
+        return learning_rate * it / warmup_iters
+    # 2) if it > lr_decay_iters, return min learning rate
+    if it > lr_decay_iters:
+        return min_lr
+    # 3) in between, use cosine decay down to min learning rate
+    decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff ranges 0..1
+    return min_lr + coeff * (learning_rate - min_lr)
+
+def train_and_evaluate_nanoGPT(
+    model: nn.Module,
+    block_size: int,
+    batch_size: int,
+    optimizer: torch.optim = None,
+    num_train_steps: int = 10000,
+    verbosity_len: int = 1000,
+    eval_iters: int = 500,
+    plot_loss: str = True,
+    device: str = "cpu",
+    **kwargs
+):
+    model.train()
+    if optimizer is None:
+        optimizer = configure_optimizers(model,weight_decay=0.01,lr=6e-4,betas=(0.9,0.95))
+        #torch.optim.AdamW(
+        #    model.parameters(), lr=kwargs["learning_rate"]
+        #)
+
+    train_losses = [np.inf]
+    valid_losses = [np.inf]
+
+    for iter in tqdm(range(num_train_steps)):
+        lr = get_lr(iter) if decay_lr else learning_rate
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+        # sample a batch of data
+        xb, yb = get_batch('train', block_size, batch_size, device)
+
+        # evaluate loss on the batch
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+
+        # gradient update
+        loss.backward()
+        optimizer.step()
+
+        # every once in a while evaluate the loss on train and val sets
+        if iter % verbosity_len == 0 or iter == num_train_steps - 1:
+            _losses = estimate_loss(train_data, valid_data, model, eval_iters)
+            train_losses.append(_losses['train'])
+            valid_losses.append(_losses['valid'])
+            print()
+            print(
+                f"step {iter}: train loss {_losses['train']:.4f}, "\
+                f"val loss {_losses['valid']:.4f}"
+            )
+
+    if plot_loss:
+        plot_losses(train_losses, verbosity_len, valid_losses)
 
 def generate_from_model(
     model: nn.Module,
